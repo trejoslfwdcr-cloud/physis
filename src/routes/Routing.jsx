f3e6699f511b480a24e2174275a94041a33b7340
trejoslfwdcr-cloud <trejoslfwdcr@gmail.com>
2026-03-16 15:02:00 -0600

    estructura archivos

--- src/routes/Routing.jsx
+++ src/routes/Routing.jsx
@@ -1,11 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Inicio from "../pages/Inicio";
+import Login from "../pages/LoginPage";
+import Register from "../pages/RegisterPage";
+import UserProfile from "../pages/UserProfile";
+import AdminProfile from "../pages/AdminProfile";
+import Resources from "../pages/Resources";
+
 
 function Routing() {
     return(
         <Router>
             <Routes>
                 <Route path="/" element={<Inicio/>}/>
+                <Route path= "/login" element={<Login/>}/>
+                <Route path= "/register" element={<Register/>}/>
+                <Route path= "/user-profile" element={<UserProfile/>}/>
+                <Route path= "/admin-profile" element={<AdminProfile/>}/>
+                <Route path= "/resources" element={<Resources/>}/>
             </Routes>
         </Router>
     )
